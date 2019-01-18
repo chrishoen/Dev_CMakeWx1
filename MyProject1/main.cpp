a9eefc14e6f1bf3b7906af20ea3ad9000af6be7b
chrishoen <chris123hoen@gmail.com>
2019-01-18 13:36:54 -0500

    more

--- MyProject1/main.cpp
+++ MyProject1/main.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <Windows.h>
 #include <cstdio>
 
 #include "MyClass1.h"
@@ -6,9 +7,18 @@
 
 int main(int argc, char** argv)
 {
+   // Initialze winsock.
+   WORD    Version = MAKEWORD(2, 2);
+   WSADATA Data;
+   WSAStartup(Version, &Data);
+
    printf("main\n");
    MyClass1 tMyClass1;
    tMyClass1.doSomething();
    doMyFunction1();
+
+   // Press enter.
+   printf("press enter\n");
+   getchar();
    return 0;
 }
