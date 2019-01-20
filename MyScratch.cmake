1d85bfd4725879c89497deb99250542f9244823b
chrishoen <chris123hoen@gmail.com>
2019-01-20 11:36:55 -0500

    more

---
+++ MyScratch.cmake
@@ -0,0 +1,13 @@
+#include(CMakeParseArguments)
+
+macro(MACRO1 _arg)
+  message("_arg1 ${${_arg}}")
+  set(${_arg} "abc")
+  message("_arg2 ${${_arg}}")
+endmacro()
+
+function(function1 _arg)
+  message("_arg1 ${${_arg}}")
+  set(${_arg} "abc")
+  message("_arg2 ${${_arg}}")
+endfunction()
